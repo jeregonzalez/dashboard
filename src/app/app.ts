45b3981d8f2dd8ed597f14d4814845e7d1ceba2e
Jeremias Gonzalez <jeremias.gonzalez@feverup.com>
2025-11-02 10:41:02 -0300

    Split components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,42 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Header } from './header/header';
+import { ServerStatus } from './dashboard/server-status/server-status';
+import { Traffic } from './dashboard/traffic/traffic';
+import { Tickets } from './dashboard/tickets/tickets';
+import { DashboardItem } from './dashboard/dashboard-item/dashboard-item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
+  imports: [Header, ServerStatus, Traffic, Tickets, DashboardItem],
 })
-export class App {
-  dummyTrafficData = [
-    {
-      id: 'd1',
-      value: 433,
-    },
-    {
-      id: 'd2',
-      value: 260,
-    },
-    {
-      id: 'd3',
-      value: 290,
-    },
-    {
-      id: 'd4',
-      value: 410,
-    },
-    {
-      id: 'd5',
-      value: 397,
-    },
-    {
-      id: 'd6',
-      value: 488,
-    },
-    {
-      id: 'd47',
-      value: 589,
-    },
-  ];
-  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
-  currentStatus = 'online';
-}
+export class App {}
